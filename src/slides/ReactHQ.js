7e3756c8a24d4f70475ff91629efefea81089595
0siel <93361992+0siel@users.noreply.github.com>
2024-10-14 10:55:14 -0600

    References

---
+++ src/slides/ReactHQ.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ReactHQ = () => {
+  return (
+    <div className="slide">
+      <h2>React HQ</h2>
+      <p>React Headquarters</p>
+      <p>San Francisco, CA</p>
+      <image
+        src="https://www.google.com/maps/uv?pb=!1s0x808580a7b3f3e3e7%3A0x7f1f4f1f0f1f1f!3m1!7e115!4shttps%3A%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipP1"
+        alt="React HQ"
+      />
+    </div>
+  );
+};
--- src/slides/References.js
+++ src/slides/References.js
@@ -8,6 +8,18 @@ const References = () => {
         <li>
           <a href="https://react.dev">[1] React. (s. f.). https://react.dev/</a>
         </li>
+        <li>
+          <a href="https://www.youtube.com/watch?v=8pDqJVdNa44&t=3039s">
+            {" "}
+            [2] How a a small team of develoers created React at Facebook
+          </a>
+        </li>
+        <li>
+          <a href="https://radixweb.com/blog/angular-vs-react-vs-vue">
+            [3] Angular vs React vs Vue- Javascript Framework You Need for Your
+            Business
+          </a>
+        </li>
       </ul>
     </div>
   );
